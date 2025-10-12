1d3794dd3ed9598473444ffd93bba47cf12bdd5f
Emerson-FullStackSenacPenha <emerson.agomes1@senacsp.edu.br>
2025-10-12 17:31:11 -0300

    Estilizando cabeçalho

---
+++ views/partials/header.php
@@ -0,0 +1,72 @@
+<header>
+<link rel="stylesheet" href="css/header.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap">
+
+
+
+<!-- Começo do menu -->
+<div id="menu">
+
+<div id="logo_busca" >
+
+<!-- Logo -->
+<div id="logo"></div>
+
+<div id="busca" >
+    <input type="text" placeholder=" Busque aqui..." >
+</div>
+
+</div>
+
+<!-- Icones -->
+<nav id="icons">
+
+    <ul id="lista_icons">
+
+        <li>
+            <a href=""> <div id="chefinho" ></div> </a>
+        </li>
+
+        <li>
+            <a href=""> <div id="entrega" ></div> </a>
+        </li>
+        
+        <li>
+            <a href=""> <div id="compras" ></div> </a>
+        </li>
+
+        <li>
+            <a href=""> <div id="usuario" ></div> </a>
+        </li>
+
+    </ul>
+
+    
+
+</nav>
+
+<!-- Fim do menu -->
+</div>
+
+
+
+
+
+
+
+<!-- Navegador -->
+<nav id="nav">
+
+    <ul id="lista_nav">
+
+        <li>Inicio</li>
+        <li>Personal Chefe</li>
+        <li>Marmitas</li>
+        <li>Outros Produtos</li>
+        <li>Quem Somos</li>
+
+    </ul>
+
+</nav>
+
+</header>
