c48619028874ec84a2063f6de872431c82f63feb
Emerson-FullStackSenacPenha <emerson.agomes1@senacsp.edu.br>
2025-10-14 20:29:02 -0300

    diretório de login em index

--- public/index.php
+++ public/index.php
@@ -3,17 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../views/partials/css/header.css">
     <title>Pronto & Saudavel</title>
 </head>
 <body>
 
-    <?php
-
-    require_once "../views/partials/header.php";
-
-    require_once "../views/partials/footer.php"
-
-    ?>
+    <?php require_once "../views/partials/header.php"; ?>
     
+    <?php require_once "../views/partials/footer.php" ?>
+
+
+
 </body>
 </html>
