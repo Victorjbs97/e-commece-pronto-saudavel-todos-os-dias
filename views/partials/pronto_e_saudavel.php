82fe8fcfc060998bee9b5e2b4b116189322bd9bc
Amorim_dev <guilhermeamorimrochalima@gmail.com>
2025-10-16 17:40:15 -0300

    correção da api e sessão pronto_e_saudavel

--- views/partials/pronto_e_saudavel.php
+++ views/partials/pronto_e_saudavel.php
@@ -4,23 +4,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sessão Pronto e Saudavel</title>
+    <link rel="stylesheet" href="../partials/css/mapa.css"> 
 </head>
 <body>
 
+    <!-- Título e link para o endereço -->
     <h3><a href="https://www.google.com/maps/place/R.+Alto+Gar%C3%A7as,+60+-+Vila+Guilhermina,+S%C3%A3o+Paulo+-+SP,+03546-000/@-23.5296909,-46.5115968,17z/data=!3m1!4b1!4m6!3m5!1s0x94ce60a6aa2ccf2b:0xa886b43ed9ecb4eb!8m2!3d-23.5296909!4d-46.5115968!16s%2Fg%2F11ts04znng?entry=ttu&g_ep=EgoyMDI1MTAxNC4wIKXMDSoASAFQAw%3D%3D">Rua Alto Garças, 60</a></h3>
 
-    <div class="mapa-container">
-        <iframe 
-            class="mapa-iframe"
-            loading = "lazy"
-            allowfullscreen
-            referrerpolicy="no-referrer-when-downgrade"
-            src="<?php echo htmlspecialchars($embedUrl); ?>"
-        </iframe>
-    </div>
-
-    <div>
-        <img src="../partials/img/fachda_pronto_e_saudavel.jpg" alt="Fachada da Marmitaria Pronto e Saudavel">
+    <div class="content-wrapper"> 
+        
+        <div class="mapa-container">
+        
+            <iframe 
+                class="mapa-iframe"
+                loading="lazy"
+                allowfullscreen
+                referrerpolicy="no-referrer-when-downgrade"
+                src="https://www.google.com/maps/embed/v1/place?key=AIzaSyC2WY_u9XulfnmdLmtjYy59N4a5HdFgdpc&q=Rua+Alto+Gar%C3%A7as%2C+60"
+            ></iframe>
+        </div>
+        
+       
+        <div class="image-container"> 
+            <img src="../partials/img/fachda_pronto_e_saudavel.jpg" alt="Fachada da Marmitaria Pronto e Saudavel">
+        </div>
     </div>
     
 </body>
