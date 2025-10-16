2c1ffd0875f5e6f80d3c788b4875100e6966c366
G-Amorim-DEV <guilhermeamorimrochalima@gmail.com>
2025-10-16 11:22:11 -0300

    feat: Implemtentação da Visualização do Mapa com API do Google Maps
    Adiciona a funcionalidade de exibição do mapa para a localização do endereço da marmitaria, utilizando a API de embed do Google Maps.
    
    - Criação do script mapa.php para construir a url do mapa usando variaveis e chave de API.
    
    -Criação da sessão pronto_e_saudavel.php dentro de um <iframe>
    
    - Garante a correta codificação e passagem dos parametros (endereço e chave).

---
+++ views/partials/pronto_e_saudavel.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sessão Pronto e Saudavel</title>
+</head>
+<body>
+
+    <h3><a href="https://www.google.com/maps/place/R.+Alto+Gar%C3%A7as,+60+-+Vila+Guilhermina,+S%C3%A3o+Paulo+-+SP,+03546-000/@-23.5296909,-46.5115968,17z/data=!3m1!4b1!4m6!3m5!1s0x94ce60a6aa2ccf2b:0xa886b43ed9ecb4eb!8m2!3d-23.5296909!4d-46.5115968!16s%2Fg%2F11ts04znng?entry=ttu&g_ep=EgoyMDI1MTAxNC4wIKXMDSoASAFQAw%3D%3D">Rua Alto Garças, 60</a></h3>
+
+    <div class="mapa-container">
+        <iframe 
+            class="mapa-iframe"
+            loading = "lazy"
+            allowfullscreen
+            referrerpolicy="no-referrer-when-downgrade"
+            src="<?php echo htmlspecialchars($embedUrl); ?>"
+        </iframe>
+    </div>
+
+    <div>
+        <img src="../partials/img/fachda_pronto_e_saudavel.jpg" alt="Fachada da Marmitaria Pronto e Saudavel">
+    </div>
+    
+</body>
+</html>
