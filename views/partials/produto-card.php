dda40cc85abe7c2247e374c6853e95c70fe7b38c
PedroHenriqueph7 <pedrohenrizzx7@gmail.com>
2025-10-17 17:41:21 -0300

    feat: Criar a estrutura do card de produtos em html + adicionar o arquivo de sessao_mais_vendidos

---
+++ views/partials/produto-card.php
@@ -0,0 +1,39 @@
+
+<link rel="stylesheet" href=css/produto_card.css">
+
+<article class="produto_card_info">
+
+    <figure class="imagem_produto">
+        <img src="" alt="">
+    </figure>
+
+    <div class="conteudo_produto">
+
+        <div class="informacoes_nutricionais">
+            <p>CONTÉM GLUTÉN</p>
+            <p>CONTÉM LACTOSE</p>
+            <p>VEGETARIANO</p>
+       </div>
+
+        <div class="fontes_de_calorias">
+
+            <div class="fontes">444<br>KCAL</div>
+
+            <div class="fontes">56<br>PROT.</div>
+
+            <div class="fontes">120<br>CARB.</div>
+
+            <div class="fontes">17<br>GORD.</div>
+
+        </div>
+
+        <h3>Marmita LowCarb</h3>
+        <p>R$ 27,00</p>
+
+        <div class="botoes_de_acoes">
+            <button class="botao_quantidade"></button>
+            <button class="botao_adicionar"></button>
+        </div>
+    </div>
+    
+</article>
