d2713e163f8a5cf74d55a704af56f82e59ee7d03
G-Amorim-DEV <guilhermeamorimrochalima@gmail.com>
2025-10-18 15:37:17 -0300

    refactor(pages): Integra sessão 'pronto_e_saudavel' e corrige assets
    Refatora o arquivo 'pronto_e_saudavel.php' para corrigir o caminho relativo
    da imagem da fachada da marmitaria, garantindo o carregamento correto.
    
    Cria a pasta 'img' dentro da pasta 'pages' para centralizar os assets da seção.
    Inclui a sessão no 'index.php' via 'require_once'.
    
    ATENÇÃO: É necessário realizar ajustes de front-end (CSS) na seção
    integrada, pois o layout está sendo cortado dentro do 'index.php'.

--- public/index.php
+++ public/index.php
@@ -4,13 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../views/partials/css/header.css">
+    <link rel="stylesheet" href="../views/partials/css/mapa.css">
     <title>Pronto & Saudavel</title>
 </head>
 <body>
 
-    <?php require_once "../views/partials/header.php"; ?>
+    <?php require_once "../views/partials/header.php" ?>;
+
+    <?php require_once "../views/partials/pronto_e_saudavel.php" ?>;
     
-    <?php require_once "../views/partials/footer.php" ?>
+    <?php require_once "../views/partials/footer.php" ?>;
 
 </body>
 </html>
