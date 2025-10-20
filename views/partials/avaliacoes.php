42de60365bdd6faf0b4b05ca79691661c2e2cfac
Victorjbs97 <Victorjbs97@gmail.com>
2025-10-20 10:25:03 -0300

    avalições

---
+++ views/partials/avaliacoes.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/avaliacaoes.css">
+    <title>Document</title>
+</head>
+<body>
+    <!-- Vai ser a posição do Avaliacões -->
+    <section class="sessao_avaliacoes">
+        <h2>Avaliacões</h2>
+        <div class="carrossel">
+            <button class="btn_voltar" aria-label="Voltar avaliação">&lt;</button>
+            <div class="carrossel_avaliacoes">
+                <article class="artigo_avaliacao"><p>A comida é de verdade, com aquele gostinho caseiro que faz toda a diferença na hora do almoço.</p></article>
+                <article class="artigo_avaliacao"><p>Pra quem busca praticidade sem perder a qualidade, eu super recomendo!</p></article>
+                <article class="artigo_avaliacao"><p>Ter uma refeição completa, balanceada e tão saborosa esperando por você é o melhor investimento no meu dia a dia.</p></article>
+                <article class="artigo_avaliacao"><p>4 Ter uma refeição completa, balanceada e tão saborosa esperando por você é o melhor investimento no meu dia a dia.</p></article>
+                <article class="artigo_avaliacao"><p>5 Ter uma refeição completa, balanceada e tão saborosa esperando por você é o melhor investimento no meu dia a dia.</p></article>
+                <article class="artigo_avaliacao"><p>6 Ter uma refeição completa, balanceada e tão saborosa esperando por você é o melhor investimento no meu dia a dia.</p></article>
+            </div>
+            <button class="btn_avancar" aria-label="Proxima avaliação">&gt;</button>
+        </div>
+        <script src="./js/avaliacao.js"></script>
+    </section>
+ </body>
+</html>
