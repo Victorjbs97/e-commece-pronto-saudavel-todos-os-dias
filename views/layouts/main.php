ce60115267eaf0df802299ffc7b815de0d7474b3
PedroHenriqueph7 <pedrohenrizzx7@gmail.com>
2025-10-31 14:50:34 -0300

    feat: Inserir imagens na galeria de fotos

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -24,6 +24,7 @@ $baseUrl = rtrim(str_replace('/public', '', $baseUrl), '/');
     <link rel="stylesheet" href="<?= $baseUrl ?>/public/css/galeria-de-fotos.css">
     <link rel="stylesheet" href="<?= $baseUrl ?>/public/css/avaliacaoes.css">
     <link rel="stylesheet" href="<?= $baseUrl ?>/public/css/mapa.css">
+    <link rel="stylesheet" href="<?= $baseUrl ?>/public/css/sessao_personal_chefe.css">
     <link rel="stylesheet" href="<?= $baseUrl ?>/public/css/atendimento-personalizado.css">
 
 </head>
@@ -35,6 +36,6 @@ $baseUrl = rtrim(str_replace('/public', '', $baseUrl), '/');
     
    
     <script src="<?= $baseUrl ?>/public/js/carrossel-produtos.js"></script>
-    <script src="/public/js/avaliacao.js"></script>
+    <script src="<?= $baseUrl ?>/public/js/avaliacao.js"></script>
 </body>
 </html>
--- views/partials/galeria-de-fotos.php
+++ views/partials/galeria-de-fotos.php
@@ -1,12 +1,18 @@
 <section class="galeria">
     
-    <h2>Galeria de Fotos</h2>
+    <h2><b>Galeria de Fotos</b></h2>
 
     <div class="galeria-fotos">
 
-        <div class="foto"></div>
-        <div class="foto"></div>
-        <div class="foto"></div>
+        <div class="foto">
+            <img src="../public/img/galeriafoto1.png" alt="">
+        </div>
+        <div class="foto">
+            <img src="../public/img/galeriafoto2.png" alt="">
+        </div>
+        <div class="foto">
+            <img src="../public/img/galeriafoto3.png" alt="">
+        </div>
         
     </div>
 
