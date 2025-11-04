a257420c6f48e82ee985bc8753ef48105e8de2fd
Kenny <shadow.dio.shadow@gmail.com>
2025-11-03 23:25:15 -0300

    Rodapé inserido
    Rodapé inserido junto de seu css e imagens

---
+++ views/partials/footer.php
@@ -0,0 +1,29 @@
+<footer id="footer">
+<link rel="stylesheet" href="css/footer.css">
+  <div class="bloco-footer"> 
+      <h3>Contato</h3>
+      <p>Cel: <a href="tel:+5511940367571">+55 11 94036-7571</a></p>
+      <p>Email: <a href="mailto:prontoesaudavel@gmail.com">prontoesaudavel@gmail.com</a></p>
+  </div>
+
+  <div class="bloco-footer"> 
+      <h3>Formas de Entrega:</h3>
+      <p>Retiradas e Entregas<br>apenas nos Sábados</p>
+      <div class="icon-local">📍</div>
+  </div>
+
+  <div class="bloco-footer pagamento"> 
+      <h3>Formas de Pagamento:</h3>
+      <div id="pagamento-footer">
+        <img src="<?php echo 'img/icons/pix.png'; ?>" alt="Pix">
+        <img src="<?php echo 'img/icons/dinheiro.png'; ?>" alt="Dinheiro">
+        <img src="<?php echo 'img/icons/debito.png'; ?>" alt="Cartão de débito">
+        <img src="<?php echo 'img/icons/credito.png'; ?>" alt="Cartão de crédito">
+      </div>
+  </div>
+
+  <div class="bloco-footer"> 
+      <h3>Dúvidas Frequentes (FAQ)</h3>
+      <a href="#">Sugestões</a>
+  </div>
+</footer>
