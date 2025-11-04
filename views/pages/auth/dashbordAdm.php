d93af7e741dac8660ff8ebdeedfc3e14a3af0878
Victorjbs97 <Victorjbs97@gmail.com>
2025-11-04 13:57:53 -0300

    correção de texto

--- views/pages/auth/dashbordAdm.php
+++ views/pages/auth/dashbordAdm.php
@@ -9,7 +9,7 @@
         $nome = trim($_POST['nome']??'');
         $descricao = trim($_POST['descricao']??'');
         $valor = filter_input(INPUT_POST, 'valor',FILTER_VALIDATE_FLOAT);
-        $estoque = filter_input(INPUT_POST, 'estoqur',FILTER_VALIDATE_INT);
+        $estoque = filter_input(INPUT_POST, 'estoque',FILTER_VALIDATE_INT);
         $imagem_url = trim($_POST['imagem_url']??'');
 
         cadastrarProduto($conexao,$nome,$descricao,$valor,$estoque,$imagem_url);
